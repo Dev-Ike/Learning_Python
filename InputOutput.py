018eda8a6dfdc636f00640bd94a6f7dd9062d71f
Ikechukwu Celestine <devikecele@gmail.com>
2025-06-07 22:13:35 +0100

    Second Lesson

---
+++ InputOutput.py
@@ -0,0 +1,18 @@
+# input()
+# accept user input
+
+#user_input = input("Enter something: ")
+#print("You entered: ", user_input)
+
+# input function returns the value as string
+
+#print(type(user_input))
+
+# int(), float()
+
+name = input("What is your name?")
+print("Hello ", name)
+
+age = int(input("Enter your age?"))
+#string converted to int
+print("Your age will be", age + 1 , "years older next year")
