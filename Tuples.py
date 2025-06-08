eacc526796e31a1cb0e84c317c1d5bce6553fdfc
Ikechukwu Celestine <devikecele@gmail.com>
2025-06-08 04:09:16 +0100

    Fourth Lesson

---
+++ Tuples.py
@@ -0,0 +1,16 @@
+#syntax
+
+#tuplename = (element1, element2, element3 ...)
+#for tuples we use normal brackets
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)
+
+#access tuple elements with index
+print(my_tuple[1])
+
+#We don't append in tuples we only concatenate
+
+tuple_2 = (6, 7)
+new_tuple = my_tuple + tuple_2
+print(new_tuple)
+#When a tuple is created you cannot change it contents but can create a new tuple for modification
