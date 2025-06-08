4d44f40963ba9b35147e593479755c3a282307a0
Ikechukwu Celestine <devikecele@gmail.com>
2025-06-08 04:11:11 +0100

    Sixth Lesson

---
+++ Dictionary.py
@@ -0,0 +1,22 @@
+#Dictionary
+
+#Syntax
+
+#dict = {key1:value1, key2:value2, key3:value3...}
+
+my_dict = {"name": "Alice", "age": 30, "city": "New York"}
+
+print(my_dict["name"])
+
+#Adding new values pairs
+
+my_dict["job"] = "Computer Science"
+print(my_dict)
+
+#Remove a key value pair
+del my_dict["age"]
+print(my_dict)
+
+#Replace value of a key
+my_dict["city"] = "Paris"
+print(my_dict)
