7a6a5809d1dc402479c7668a9087042ae97c2f98
Ikechukwu Celestine <devikecele@gmail.com>
2025-06-08 22:55:13 +0100

    Eight Lesson

---
+++ Operators.py
@@ -0,0 +1,70 @@
+# Arithemetic Operations
+
+# Add, Subtract, Multiply, Modulus, Exponent, Floor division
+# +,-,*,/,%,**,//
+
+
+x=10
+y=3
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+print(x//y)
+
+
+# Comparison Operators
+
+# >,<, ==, !=, >=, <=
+
+print(x>y)
+print(x<y)
+print(x==y)
+print(x!=y)
+print(x>=y)
+print(x<=y)
+
+
+
+
+# Logical Operators
+# and, or, not
+
+a,b = True, False
+
+print(a and b)
+print(a or b)
+print(not a)
+
+
+# Assignment Operators
+# +=, -=, *=, /=
+
+x=5
+
+x += 2 # x = x+2
+x -= 3 # x = x-3
+x *= 5 # x = x*5
+x /= 2 # x = x/2
+
+
+
+# Membership Operators
+# in, not in
+
+nums = [1, 2, 3]
+print(2 in nums) # True
+print(8 not in nums) # True
+
+
+# Identity Operators
+# is, is not
+
+x = [1, 2, 3]
+y = x
+
+print(x is y) # True
+print(x is not y) # False
