caafe0d5a95f0f19e9d9a02f8008e6ab73ebc336
Ikechukwu Celestine <devikecele@gmail.com>
2025-06-09 17:52:30 +0100

    Eleventh Lesson

---
+++ LogicalOperators.py
@@ -0,0 +1,24 @@
+# and
+
+# c1 and c2
+
+age = 20
+citizenship = True
+if age >= 18 and citizenship:
+    print("Eligible to vote")
+
+# or
+
+age = 17
+has_permissions = True
+if age >= 18 or has_permissions:
+    print("You can go to the concert")
+
+
+# Not
+# not false = true
+# not true = false
+
+is_sunny = False
+if not is_sunny:
+    print("It is cloudy")
