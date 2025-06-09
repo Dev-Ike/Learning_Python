48a1e73c898edb07fc632db5c15ff163a9073120
Ikechukwu Celestine <devikecele@gmail.com>
2025-06-09 17:51:49 +0100

    Tenth Lesson

---
+++ ControlFlow.py
@@ -0,0 +1,17 @@
+# if, elif, else
+
+# Control execution flow
+
+# if condition:
+    # code to execute if condition is true
+
+x = 0
+if x > 0:
+    print("x is positive")
+elif x == 0:
+    print("x is zero")
+else:
+    print("x is negative")
+
+# x = 0
+#elif - else if
