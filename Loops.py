b54a2da6534938a5cffef8dd83a206cf7e2e6448
Ikechukwu Celestine <devikecele@gmail.com>
2025-06-09 18:22:09 +0100

    Twelfth Lesson

---
+++ Loops.py
@@ -0,0 +1,30 @@
+# for loop
+
+# 5 books
+# b1, b2, b3, b4, b5
+
+for i in range(5):
+    print(i)
+
+# i is iterated from 0 - 4
+# 5 times
+
+
+
+# while loop
+
+# condition is true
+
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+
+# 5 iterations
+# count = 0
+# count = 1
+# count = 2
+# count = 3
+# count = 4
+# count = 5 becomes false
+
