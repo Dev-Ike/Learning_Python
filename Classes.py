7fe35ae9f4f95ea24f68270c980ee2ea3939d763
Ikechukwu Celestine <devikecele@gmail.com>
2025-06-16 03:28:36 +0100

    Sixteenth Lesson

---
+++ Classes.py
@@ -0,0 +1,30 @@
+class Car:
+
+    location = "Europe"
+
+    def __init__(self, wheels, engine, color):
+        self.wheels = wheels
+        self.engine = engine
+        self.color = color
+
+    def display_info(self):
+        print("Car Wheels:", self.wheels)
+        print("Car Engine:", self.engine)
+        print("Car Color:", self.color)
+
+bmw = Car(4, "Hybrid", "Blue")
+ferrari = Car(4, "Fuel", "Red")
+
+bmw.display_info()
+ferrari.display_info()
+
+bmw.seats = 2
+
+ferrari.gears = 9
+ferrari.speed = 350
+
+print(f"BMW - Seats: {bmw.seats}")
+print(f"Ferrari - Gears: {ferrari.gears}, Speed: {ferrari.speed}")
+
+print("Location BMW", bmw.location)
+print("Location Ferrari", ferrari.location)
